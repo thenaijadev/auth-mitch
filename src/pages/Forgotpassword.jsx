76a2555d8e55b56830062a34952a89b88b5c45f3
mitchellin <mitchell.tessy@gmail.com>
2022-05-30 20:58:01 -0700

    first

--- src/pages/Forgotpassword.jsx
+++ src/pages/Forgotpassword.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function forgotpassword() {
   return (
     <div className='w-full h-auto flex justify-center items-center bg-gray-200'>
-        <div className='w-96 h-auto bg-white shadow-md rounded-lg p-4 flex flex-col mt-14'>
+        <div className='w-96 h-auto bg-white shadow-md rounded-2xl p-4 flex flex-col mt-14'>
             <h3 className='text-black text-2xl font-bold text-center'>HerTech</h3>
             <p className='text-gray-600 text-md font-light text-center mt-2'>Forgot password</p>
 
-            <input Type="email" placeholder='example@mail.com' className='w-full h-10 rounded-lg border-2 border-gray-500 px-8 mt-4' />
+            <input Type="email" placeholder='email address' className='w-full h-10 rounded-lg border-2 border-gray-500 px-8 mt-4' />
 
 
             <button className='w-full h-12 rounded-lg bg-black text-white font-bold text-center mt-5'>Reset password</button>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,19 +6,22 @@ import linkedIn from "../images/linkedIn.svg";
 export default function signup() {
   return (
     <div className='w-full h-auto flex justify-center items-center bg-gray-200'>
-        <div className='w-96 h-auto bg-white shadow-md rounded-lg p-4 flex flex-col mt-14'>
+        <div className='w-96 h-auto bg-white shadow-md rounded-3xl p-4 flex flex-col mt-14'>
             <h3 className='text-black text-2xl font-bold text-center'>HerTech</h3>
-            <p className='text-gray-800 text-md font-light text-center mt-2'>You're 1-click away from creating an account</p>
+            <p className='text-black text-md font-medium text-center mt-2'>You're 1-click away from creating an account</p>
 
-            <input Type="email" placeholder='example@mail.com' className='w-full h-10 rounded-lg border-2 border-gray-500 px-8 mt-4' />
+            <input Type="email" placeholder='email address' className='w-full h-10 rounded-lg border-2 border-gray-500 px-8 mt-4' />
+            <div className='relative'>
             <input Type="password" placeholder='password' className='w-full h-10 rounded-lg border-2 border-gray-500 px-8 mt-4' />
+            <span className='text-right absolute right-4 bottom-0 top-6 left-8 text-gray-500 font-xs'>show</span>
+            </div>
 
 
             <button className='w-full h-12 rounded-lg bg-black text-white font-bold text-center mt-5'>Login to HerTech</button>
 
-            <p className='text-center mt-6 font-light text-lg text-gray-600'>OR</p>
+            <p className='text-center mt-5 font-light text-lg text-gray-600'>OR</p>
 
-            <button className='w-full h-12 rounded-lg bg-gray-200 text-black items-center justify-center flex font-medium text-center mt-5 '> <img src={google} alt='google-icon' className=' p-3' /> Continue with Google</button>
+            <button className='w-full h-12 rounded-lg bg-gray-200 text-black items-center justify-center flex font-medium text-center mt-3 '> <img src={google} alt='google-icon' className=' p-3' /> Continue with Google</button>
             <button className='w-full h-12 rounded-lg bg-gray-200 text-black items-center justify-center font-medium flex mt-3 mb-5'><img src={linkedIn} alt='linkedIn-icon' className='p-3'/> Continue with LinkedIn</button>
 
 
